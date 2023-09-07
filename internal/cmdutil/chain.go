0bb9c26b976ba436bd174e726fafeecdb4fd91b4
Lukas Malkmus <lukas@axiom.co>
2023-09-07 12:40:37 +0200

    fix(typo): s/overwrittes/overwrites/
    Fixes #168.

--- internal/cmdutil/chain.go
+++ internal/cmdutil/chain.go
@@ -33,11 +33,11 @@ var (
 		  $ {{ bold "axiom auth select" }}
 		  
 		  Select a deployment by setting the {{ bold "AXIOM_DEPLOYMENT" }} environment variable. This
-		  overwrittes the choice made in the configuration file: 
+		  overwrites the choice made in the configuration file: 
 		  $ {{ bold "export AXIOM_DEPLOYMENT=axiom-eu-west-1" }}
 
 		  Select a deployment by setting the {{ bold "-D" }} or {{ bold "--deployment" }} flag. This
-		  overwrittes the choice made in the configuration file or the environment: 
+		  overwrites the choice made in the configuration file or the environment: 
 		  $ {{ bold .CommandPath }} {{ bold "-D=axiom-eu-west-1" }}
 
 		  For non-interactive use, set AXIOM_TOKEN and AXIOM_URL to target a deployment directly,
